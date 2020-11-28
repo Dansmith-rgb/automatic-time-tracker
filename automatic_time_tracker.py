debd63fb5a09ca37284ad0fea14d6ee79eaa32ec
Dan <68809853+Dansmith-rgb@users.noreply.github.com>
2020-11-28 10:40:55 +0000

    Add files via upload

---
+++ automatic_time_tracker.py
@@ -0,0 +1,47 @@
+from win32gui import GetWindowText, GetForegroundWindow
+import os
+import selenium
+import plotly
+import subprocess
+import time
+import datetime
+win = []
+active_window_name = []
+next_window_name = active_window_name
+first = True
+first2 = True
+stop = datetime.datetime.now()
+start = datetime.datetime.now()
+timelist = []
+try:
+    #bob2 = GetWindowText(GetForegroundWindow())
+    while True:
+        #active_window_name.append(GetWindowText(GetForegroundWindow()))
+        bob2 = GetWindowText(GetForegroundWindow())
+        bob = GetWindowText(GetForegroundWindow())
+        if bob == bob2 and first:
+            start = datetime.datetime.now()
+            print(start)
+            timelist.append(start)
+            first = False
+        #if bob != bob2 and not first:
+            #stop = datetime.datetime.now()
+            #timelist.append(stop)
+            #print(stop)
+            #first = True
+            #break
+        if bob != bob2 and not first:
+            stop = datetime.datetime.now()
+            timelist.append(stop)
+            print(stop)
+            print("hello")
+            active_window_name.append(GetWindowText(GetForegroundWindow()))
+            print(active_window_name)
+            first = True
+        
+
+except KeyboardInterrupt:
+    stop = datetime.datetime.now()
+    timelist.append(stop)
+    print(stop)
+    print("OOPs")
